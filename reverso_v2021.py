c98fb3b486655bc7483f2d079eab6d1628680f59
Sergey Shirnin <66397172+BWS-code@users.noreply.github.com>
2022-02-04 13:57:28 +1100

    Stage 6 better
    added plurals handling
    added spacer handler
    added languages getter (yet will not pass stage - commented

--- reverso_v2021.py
+++ reverso_v2021.py
@@ -3,30 +3,19 @@ from bs4 import BeautifulSoup
 from time import sleep
 from functools import reduce
 import re
+from support import *
 
 
 class Reverso_translator:
-    def __init__(self, meta, translations=5, examples=5, examples_to_pairs=True, to_file=None):
+    def __init__(self, terminal_inp, meta, languages,
+                 translations=5, examples=5, examples_to_pairs=True,
+                 to_file=None):
         self.url = 'https://context.reverso.net/translation/'
-        self.to_lang = self.from_lang = self.word = None
+        self.from_lang, self.to_lang = terminal_inp['from_lang'], terminal_inp['to_lang']
+        self.word = terminal_inp['word']
         self.translations_links = []
         self.translations_languages = []
-        self.lang_dict = {
-            '1': 'Arabic',
-            '2': 'German',
-            '3': 'English',
-            '4': 'Spanish',
-            '5': 'French',
-            '6': 'Hebrew',
-            '7': 'Japanese',
-            '8': 'Dutch',
-            '9': 'Polish',
-            '10': 'Portuguese',
-            '11': 'Romanian',
-            '12': 'Russian',
-            '13': 'Turkish'
-        }
-        # self.lang_dict = None
+        self.lang_dict = languages
         # self.lang_tag = meta['languages_tag']
         # self.lang_tag_attr = meta['languages_tag_attr']
         # self.lang_tag_attr_len = meta['languages_tag_attr_len']
@@ -37,7 +26,7 @@ class Reverso_translator:
         self.ex_tag = meta['example_tag']
         self.ex_cl = meta['example_class']
         self.translations = translations
-        self.examples = examples * 2
+        self.examples = examples
         self.examples_to_pairs = examples_to_pairs
         self.to_file = to_file
         self.results_string = ''
@@ -50,9 +39,10 @@ class Reverso_translator:
     #         lambda tag: tag.name == self.lang_tag and \
     #                     tag.get(self.lang_tag_attr) and \
     #                     len(tag.get(self.lang_tag_attr)) == self.lang_tag_attr_len)]))
+    #     languages.sort()
     #     for i, lang in enumerate(('English', 'Russian')):
     #         languages.insert(i, languages.pop(languages.index(lang)))
-    #     self.lang_dict = {str(i + 1): el for i, el in enumerate(languages)}
+    #     self.lang_dict = {'0': 'All'} | {str(i + 1): el for i, el in enumerate(languages)}
 
     def get_welcome(self):
         print('Hello, you\'re welcome to the translator. Translator supports: ')
@@ -61,24 +51,20 @@ class Reverso_translator:
 
     def get_input(self):
         self.from_lang = self.lang_dict[input('Type the number of your language:')]
-        to_lang = input(
-            'Type the number of language you want to translate to:')
-        if not to_lang == '0':
-            self.to_lang = self.lang_dict[to_lang]
-        else:
-            self.to_lang = 'all'
-            self.translations = self.examples = 1
+        to_lang = input('Type the number of language you want to translate to:')
+        self.to_lang = self.lang_dict[to_lang]
         self.word = input('Type the word you want to translate:')
         print()
 
     def get_links(self):
-        if not self.to_lang == 'all':
+        if not self.to_lang == 'All':
             self.translations_links.append(
                 self.url + self.from_lang.lower() + '-' + self.to_lang.lower() + f'/{self.word}')
             self.translations_languages.append(self.to_lang)
         else:
+            self.translations = self.examples = 1
             for _, to_lang in self.lang_dict.items():
-                if not self.from_lang == to_lang:
+                if not to_lang == 'All' and not self.from_lang == to_lang:
                     self.translations_links.append(
                         self.url + self.from_lang.lower() + '-' + to_lang.lower() + f'/{self.word}')
                     self.translations_languages.append(to_lang)
@@ -99,10 +85,10 @@ class Reverso_translator:
     def get_examples(self):
         if self.examples:
             examples = [el.text.strip() for el in self.soup.find(self.exs_tag, id=self.exs_id).findAll(
-                self.ex_tag, class_=self.ex_cl)][:self.examples]
+                self.ex_tag, class_=self.ex_cl)][:self.examples * 2]
             if self.examples_to_pairs:
                 paired_examples = reduce(lambda x, y: x + y, [[el, ''] if i % 2 and i != len(examples) - 1
-                                               else [el] for i, el in enumerate(examples)])
+                                                              else [el] for i, el in enumerate(examples)])
                 return paired_examples
             return examples
         return None
@@ -126,10 +112,11 @@ class Reverso_translator:
         with open(f'{self.word}.txt', 'wb') as file:
             file.write(self.results_string.encode('UTF-8'))
 
-    def main(self):
+    def run(self):
         # self.get_languages()
-        self.get_welcome()
-        self.get_input()
+        if self.word is None:
+            self.get_welcome()
+            self.get_input()
         self.get_links()
         for link, language in zip(self.translations_links, self.translations_languages):
             self.to_lang = language
@@ -137,26 +124,34 @@ class Reverso_translator:
             self.get_soup()
             translations = self.get_translations()
             examples = self.get_examples()
-            self.update_results(translations, heading=f'Translation{"s"[:self.translations^1]}')
-            self.update_results(examples, heading=f'Example{"s"[:self.examples^1]}')
+            self.update_results(translations, heading=f'Translation{"s"[:self.translations ^ 1]}')
+            self.update_results(examples, heading=f'Example{"s"[:self.examples ^ 1]}')
         print(self.results_string)
         if self.to_file:
             self.save_txt()
 
-known_ids = {
-    # 'languages_tag': 'span',
-    # 'languages_tag_attr': 'data-value',
-    # 'languages_tag_attr_len': 2,
-    'translations_tag': 'a',
-    'translations_class_regex': 'translation.*ltr|rtl',
-    'examples_tag': 'section',
-    'examples_id': 'examples-content',
-    'example_tag': 'span',
-    'example_class': 'text'
-}
+def terminal_error():
+    print(f'{FAIL}<!!FAILED> wrong call or language\nExpected >python file.py from_language to_language which_word{ENDC}')
+    print(f'Supported >', ', '.join(language.lower() for _, language in supported_languages.items()))
 
-my_translator = Reverso_translator(known_ids,
-                                   translations=5, examples=5,
-                                   examples_to_pairs=True,
-                                   to_file='pls')
-my_translator.main()
+
+def create_and_run():
+    my_translator = Reverso_translator(terminal_entry, known_ids, supported_languages,
+                                       translations=5, examples=5,
+                                       examples_to_pairs=True,
+                                       to_file='pls')
+    my_translator.run()
+
+def main():
+    if not len(arguments) in (1, 4):
+        terminal_error()
+    elif len(arguments) > 2:
+        if not terminal_entry['from_lang'] in supported_languages.values() or \
+                not terminal_entry['to_lang'] in supported_languages.values():
+            terminal_error()
+        else:
+            create_and_run()
+    else:
+        create_and_run()
+
+main()
