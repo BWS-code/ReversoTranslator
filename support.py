f84585ca73fdc706cea16311bdaa480d5f1e5beb
Sergey Shirnin <66397172+BWS-code@users.noreply.github.com>
2022-02-04 13:58:38 +1100

    Stage 6 better
    added terminal fail color

--- support.py
+++ support.py
@@ -2,6 +2,9 @@ import sys
 
 
 known_ids = {
+    # 'languages_tag': 'span',
+    # 'languages_tag_attr': 'data-value',
+    # 'languages_tag_attr_len': 2,
     'translations_tag': 'a',
     'translations_class_regex': 'translation.*ltr|rtl',
     'examples_tag': 'section',
@@ -9,17 +12,21 @@ known_ids = {
     'example_tag': 'span',
     'example_class': 'text'
 }
-arguments = sys.argv
+
 supported_languages = {
-    '0': 'all',
-    '1': 'arabic', '2': 'german',
-    '3': 'english', '4': 'spanish',
-    '5': 'french', '6': 'hebrew',
-    '7': 'japanese', '8': 'dutch',
-    '9': 'polish', '10': 'portuguese',
-    '11': 'romanian', '12': 'russian',
-    '13': 'turkish', '14': 'chinese'
+    '0': 'All',
+    '1': 'Arabic', '2': 'German', '3': 'English', '4': 'Spanish', '5': 'French',
+    '6': 'Hebrew', '7': 'Japanese', '8': 'Dutch', '9': 'Polish', '10': 'Portuguese',
+    '11': 'Romanian', '12': 'Russian', '13': 'Turkish'
 }
-user_data = {'from_lang': arguments[1] if len(arguments) == 4 else None if len(arguments) == 1 else '',
-             'to_lang': arguments[2] if len(arguments) == 4 else None if len(arguments) == 1 else '',
-             'word': arguments[3] if len(arguments) == 4 else None if len(arguments) == 1 else ''}
+
+arguments = sys.argv
+terminal_entry = {
+    'from_lang': arguments[1].capitalize() if len(arguments) == 4 else None if len(arguments) == 1 else '',
+    'to_lang': arguments[2].capitalize() if len(arguments) == 4 else None if len(arguments) == 1 else '',
+    'word': arguments[3] if len(arguments) == 4 else None if len(arguments) == 1 else ''
+}
+
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
